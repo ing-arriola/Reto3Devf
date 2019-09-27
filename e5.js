498f0a4fd6f3248d7ca4b6e872c9fdad9bb0b29e
ing-arriola <ingeniero.arriola@gmail.com>
2019-09-27 15:37:52 -0600

    PrimeraParteE5

---
+++ e5.js
@@ -0,0 +1,17 @@
+class quinto {
+  constructor(hrs) {
+    fsuperior(hrs,calculaHoras)
+  }
+}
+
+let calculaHoras=function(variasHoras){
+    let arr=[]
+    
+}
+
+let fsuperior=function(hr,callback){
+  console.log("El resultado es: "+callback(hr))
+}
+module.exports={
+  quinto
+}
--- principal.js
+++ principal.js
@@ -2,13 +2,14 @@ const {primer}=require('./e1')
 const {segundo}=require('./e2')
 const {tercer}=require('./e3')
 const {cuarto}=require('./e4')
+const {quinto}=require('./e5')
 let x= function saludar(){
     console.log("Ahora saludo desde Callback")
 }
 let z= function segundaF(){
     return "Saludo desde "
 }
-
+let horas=[120, 80, 200, 100]
 const salu=new primer(x)
 const eje2=new segundo(z,"hola")
 const eje2b=new segundo(z,"OtraCosa")
